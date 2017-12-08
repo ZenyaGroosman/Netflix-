5fbaac9c9046520025c057e399c6a53fc0881adc
Zenya Groosman <zenya_groosman@live.nl>
2017-12-08 11:40:47 +0100

    Begroetingen2.0

--- src/nl/avans/User.java
+++ src/nl/avans/User.java
@@ -2,8 +2,12 @@ package nl.avans;
 
 public class User {
     private String fullName;
+    private int age;
+    private String lastName;
 
-    public User(String fullName){
+    public User(String fullName, int age, String lastName){
         this.fullName = fullName;
+        this.age = age;
+        this.lastName = lastName;
     }
 }
