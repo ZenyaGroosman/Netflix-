10c37af01a9bd64b79c94b33677f1d7509b6f303
Arno Broeders <ahc.broeders@avans.nl>
2017-12-08 11:22:01 +0100

    Iedereen fijn weekend!

--- src/nl/avans/Main.java
+++ src/nl/avans/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hallo");
-        sayHello();
+        System.out.println("Fijn weekend!");
+        //sayHello();
     }
 
     public static void sayHello() {
