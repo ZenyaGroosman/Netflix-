62565802a6a8da788be367ac99f98df414b11a61
Zenya Groosman <zenya_groosman@live.nl>
2017-12-08 11:31:30 +0100

    Begroetingen

--- src/nl/avans/Main.java
+++ src/nl/avans/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hallo");
+        System.out.println("Goedemorgen");
+        System.out.println("Goedeavond");
         System.out.println("Doei");
         sayHello();
     }
