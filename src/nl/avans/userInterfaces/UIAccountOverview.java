49d980b4a9d2a28897935ca55233d5b2b11efd1f
Retroskater <dion008@live.nl>
2017-12-21 11:09:19 +0100

    plaatje allen ziet er niet uit

--- src/nl/avans/userInterfaces/UIAccountOverview.java
+++ src/nl/avans/userInterfaces/UIAccountOverview.java
@@ -18,18 +18,37 @@ public class UIAccountOverview extends UserInterfaceBase{
     }
 
     @Override
-        public void run() {
-        createComponents(getFrame().getContentPane());
-        getFrame().pack();
-        getFrame().setVisible(true);
+    public void run() {
+    createComponents(getFrame().getContentPane());
+    getFrame().pack();
+    getFrame().setVisible(true);
     }
+
     @Override
     public void createComponents(Container container) {
         container.setLayout(new BorderLayout(10, 5));
         container.add(createHeader(), BorderLayout.NORTH);
         container.add(createFooter(), BorderLayout.SOUTH);
-
+        titel(container);
     }
+
+    private void titel(Container container) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JPanel grid = new JPanel();
+        grid.setLayout(new GridLayout(1, 3));
+
+        panel.add(grid, BorderLayout.NORTH);
+
+
+        grid.add(new JLabel("Account Overzicht"));
+        container.add(panel);
+
+        
+    }
+
+
 
 
 
